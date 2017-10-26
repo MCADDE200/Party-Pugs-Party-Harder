164d4731c393ec50bb2f495c9c3e85004911f49f
MCADDE200 <MCADDE200@caledonian.ac.uk>
2017-10-26 10:33:44 +0100

    Version1
    First version

---
+++ Assets/Scenes/Our Scripts/TinderScript.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lean.Touch
+{
+
+    public class TinderScript : MonoBehaviour
+    {
+        int counter = 0;
+        int numAnimals = 10;
+        int score = 0;
+        int wrongAnswers = 0;
+
+        bool pug;
+        
+        public GameObject pugEntry;
+        public GameObject fakeCat;
+
+        public Text Health;
+        public Text Score;
+
+        // Use this for initialization
+        void Start()
+        {
+            chooseAnimal();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void chooseAnimal()
+        {
+            pugEntry.SetActive(false);
+            fakeCat.SetActive(false);
+            int animal = Random.Range(0, 2);
+            if(animal == 0)
+            {
+                pug = true;
+                //Debug.Log("Pug!");
+                pugEntry.SetActive(true);
+                counter++;
+            }
+            else
+            {
+                pug = false;
+                //Debug.Log("Stupid Cat!");
+                fakeCat.SetActive(true);
+                counter++;
+            }
+        }
+
+        protected virtual void OnEnable()
+        {
+            // Hook into the events we need
+            LeanTouch.OnFingerSwipe += OnFingerSwipe;
+        }
+
+        protected virtual void OnDisable()
+        {
+            // Unhook the events
+            LeanTouch.OnFingerSwipe -= OnFingerSwipe;
+        }
+
+        public void OnFingerSwipe(LeanFinger finger)
+        {
+            // Make sure the info text exists
+            //if (InfoText != null)
+            //{
+            //    // Store the swipe delta in a temp variable
+            var swipe = finger.SwipeScreenDelta;
+            if (counter < numAnimals && wrongAnswers < 3)
+            {
+                if (swipe.x < -Mathf.Abs(swipe.y))
+                {
+                    // Debug.Log("Left!");
+                    if (!pug)
+                    {
+                        score++;
+                        if (counter < numAnimals)
+                        {
+                            chooseAnimal();
+                        }
+                        else
+                        {
+                            Debug.Log(score);
+                        }
+                    }
+                    else
+                    {
+                        wrongAnswers++;
+                        if (counter < numAnimals)
+                        {
+                            chooseAnimal();
+                        }
+                        else
+                        {
+                            Debug.Log(score);
+                        }
+                    }
+                }
+
+                if (swipe.x > Mathf.Abs(swipe.y))
+                {
+                    //Debug.Log("Right!");
+                    if (pug)
+                    {
+                        score++;
+                        if (counter < numAnimals)
+                        {
+                            chooseAnimal();
+                        }
+                        else
+                        {
+                            Debug.Log(score);
+                        }
+                    }
+                    else
+                    {
+                        wrongAnswers++;
+                        if (counter < numAnimals)
+                        {
+                            chooseAnimal();
+                        }
+                        else
+                        {
+
+                            Debug.Log(score);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("Game Over! /n Your Score was: " + score + " You got " + wrongAnswers + " Answers wrong");
+            }
+
+            //if (swipe.y < -Mathf.Abs(swipe.x))
+            //{
+            //    Debug.Log("Down!");
+            //}
+
+            //if (swipe.y > Mathf.Abs(swipe.x))
+            //{
+            //    Debug.Log("Up!");
+            //}
+        }
+    }
+}
