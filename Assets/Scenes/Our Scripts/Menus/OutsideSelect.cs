263971d3cb03f4e0b20d6596466d723d81dfab9c
Maison122 <MFLEMI204@caledonian.ac.uk>
2017-11-05 13:42:03 +0000

    Updates

---
+++ Assets/Scenes/Our Scripts/Menus/OutsideSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutsideSelect : MonoBehaviour
+
+{ 
+
+    public GameObject loadingImage;
+
+    public void LoadScene(int BouncerPug)
+    {
+        loadingImage.SetActive(true);
+        Application.LoadLevel("Bouncer Pug");
+    }
+
+    
+}
---
+++ Assets/Scenes/Our Scripts/Menus/StallSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StallSelect : MonoBehaviour
+
+{ 
+
+    public GameObject loadingImage;
+
+    public void LoadScene(int SplatTheCat)
+    {
+        loadingImage.SetActive(true);
+        Application.LoadLevel("Splat The Cat");
+    }
+
+
+}
