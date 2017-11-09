b7046ad70d111fad5949e9a73f5e0ad08e2edece
MCADDE200 <MCADDE200@caledonian.ac.uk>
2017-11-09 11:50:39 +0000

    Version 5.4
    Splat the cat fixed menu assets added

--- Assets/Scenes/Our Scripts/Menus/OutsideSelect.cs
+++ Assets/Scenes/Our Scripts/Menus/OutsideSelect.cs
@@ -5,7 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class OutsideSelect : MonoBehaviour
 
-{ 
+{
+    public GameObject bouncerPugUI;
+    public GameObject carnivalStallsUI;
+
+    private void Start()
+    {
+        bouncerPugUI.SetActive(false);
+        carnivalStallsUI.SetActive(false);
+    }
+
+    public void LoadBouncerPugConfirmation()
+    {
+        bouncerPugUI.SetActive(true);
+    }
+
+    public void LoadCarnivalStallConfirmation()
+    {
+        carnivalStallsUI.SetActive(true);
+    }
 
     public void LoadBouncerPug()
     {
@@ -15,6 +33,12 @@ public class OutsideSelect : MonoBehaviour
     public void LoadCarnivalStalls()
     {
         SceneManager.LoadScene("Carnival Stalls");
+    }
+
+    public void CancelUI()
+    {
+        bouncerPugUI.SetActive(false);
+        carnivalStallsUI.SetActive(false);
     }
 
 }
