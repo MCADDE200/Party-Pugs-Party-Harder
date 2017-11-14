a8d9716de32d3dfcd1fcd73f10210a8b832814d4
MCADDE200 <MCADDE200@caledonian.ac.uk>
2017-11-14 15:48:49 +0000

    Version 6.6

--- Assets/Scenes/Our Scripts/ChaseTheBus/ChaseMovementScript.cs
+++ Assets/Scenes/Our Scripts/ChaseTheBus/ChaseMovementScript.cs
@@ -2,24 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChaseMovementScript : MonoBehaviour {
 
-
-    Rigidbody rb;
-
-	// Use this for initialization
-	void Start () {
-        rb = this.GetComponent<Rigidbody>();
-        //RunForestRun();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 5);
-    }
-
-    void RunForestRun()
+namespace Lean.Touch
+{
+    public class ChaseMovementScript : MonoBehaviour
     {
+
+
+        Rigidbody rb;
+
+        protected virtual void OnEnable()
+        {
+            // Hook into the events we need
+            LeanTouch.OnFingerSwipe += OnFingerSwipe;
+        }
+
         
+
+        // Use this for initialization
+        void Start()
+        {
+            rb = this.GetComponent<Rigidbody>();
+            //RunForestRun();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 5);
+        }
+
+        void RunForestRun()
+        {
+
+        }
+
+        public void OnFingerSwipe(LeanFinger finger)
+        {
+            var swipe = finger.SwipeScreenDelta;
+
+            if (swipe.y > Mathf.Abs(swipe.x))
+            {
+                Debug.Log("Up");
+                rb.velocity = new Vector3(rb.velocity.x, 5, rb.velocity.z);
+            }
+        }
+
+        protected virtual void OnDisable()
+        {
+            // Unhook the events
+            LeanTouch.OnFingerSwipe -= OnFingerSwipe;
+        }
     }
 }
