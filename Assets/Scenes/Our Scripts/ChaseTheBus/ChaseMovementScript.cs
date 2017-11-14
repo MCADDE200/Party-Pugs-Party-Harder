15a6906e7bce91a049fc6bdbe1205aab90562852
MCADDE200 <MCADDE200@caledonian.ac.uk>
2017-11-14 11:09:51 +0000

    Version 6.0

---
+++ Assets/Scenes/Our Scripts/ChaseTheBus/ChaseMovementScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseMovementScript : MonoBehaviour {
+
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        //RunForestRun();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 5);
+    }
+
+    void RunForestRun()
+    {
+        
+    }
+}
