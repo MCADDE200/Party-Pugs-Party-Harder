2a7664e80d56bce3e682838bf15f4d1d5520b7de
sriple200 <sriple200@caledonian.ac.uk>
2017-11-16 12:26:09 +0000

    Version 6.7

--- Assets/Scenes/Our Scripts/Menus/TicketMenuScript.cs
+++ Assets/Scenes/Our Scripts/Menus/TicketMenuScript.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class TicketMenuScript : MonoBehaviour {
+public class TicketMenuScript : MonoBehaviour
+{
+    int tickets;
+    public Text ticketText;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+
+        tickets = 0;
+ 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        ticketText.text = "Tickets: " + tickets;
 		
 	}
     public void LoadShop()
     {
         SceneManager.LoadScene("Shop");
     
+    }
+    public void AddTickets1()
+    {
+        tickets += 10;
+
+    }
+    public void AddTickets2()
+    {
+        tickets += 50;
+
+    }
+    public void AddTickets3()
+    {
+        tickets += 100;
+
+    }
+    public void AddTickets4()
+    {
+        tickets += 250;
+
+    }
+    public void AddTickets5()
+    {
+        tickets += 500;
+
     }
 }
