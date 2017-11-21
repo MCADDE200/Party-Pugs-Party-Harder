aaec390aa3b738303f0e4f75339f413a8f7c11d5
MCADDE200 <MCADDE200@caledonian.ac.uk>
2017-11-21 14:09:19 +0000

    Version 7.4

---
+++ Assets/Scenes/Our Scripts/Bouncer Pug/PugMovementScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PugMovementScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //RotatePug();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RotatePug()
+    {
+        //this.transform.Rotate(Vector3.up, -90);
+    }
+}
