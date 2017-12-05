3db3e2ca04813af89ae380faa98436855816dacd
kristiannn <k.nenov96@gmail.com>
2017-12-05 05:37:59 +0000

    Version 10.3

--- Assets/Scenes/Our Scripts/Menus/GameDataScript.cs
+++ Assets/Scenes/Our Scripts/Menus/GameDataScript.cs
@@ -6,11 +6,18 @@ using UnityEngine.SceneManagement;
 public class GameDataScript : MonoBehaviour {
 
     public int selectedSkin;
-
+    public int tickets;
+    //Power up charges
+    public int goldenBone;
+    public int crossPug;
 
     // Use this for initialization
     void Start () {
         DontDestroyOnLoad(this);
+
+        tickets = 0;
+        goldenBone = 0;
+        crossPug = 0;
     }
 	
 	// Update is called once per frame
---
+++ Assets/Scenes/Our Scripts/Menus/PowerUpsScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PowerUpsScript : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void GoldenBone()
+    {
+
+        GameObject gameData = GameObject.Find("GameData");
+        if (gameData != null)
+        {
+            if (gameData.GetComponent<GameDataScript>().tickets >= 100)
+            {
+                gameData.GetComponent<GameDataScript>().tickets -= 100;
+                gameData.GetComponent<GameDataScript>().goldenBone += 1;
+            }
+            else
+            {
+                Debug.Log("u need moar tickets bruh");
+            }
+        }
+    }
+
+    public void CrossPug()
+    {
+        GameObject gameData = GameObject.Find("GameData");
+        if (gameData != null)
+        {
+            if (gameData.GetComponent<GameDataScript>().tickets >= 100)
+            {
+                gameData.GetComponent<GameDataScript>().tickets -= 100;
+                gameData.GetComponent<GameDataScript>().crossPug += 1;
+            }
+            else
+            {
+                Debug.Log("u need moar tickets bruh");
+            }
+        }
+    }
+
+    public void TicketAmount()
+    {
+        //Output ticket amount
+    }
+}
